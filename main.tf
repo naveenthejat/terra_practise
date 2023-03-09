a971cf38ab824c6cdfd59158c067743af15cb2ab
naveenthejat <naveenthejat>
2023-03-09 16:09:56 +0530

    Your commit message here

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+
+}
+
+output "id" {
+
+  value = azurerm_resource_group.rg.id
+
+}
+
